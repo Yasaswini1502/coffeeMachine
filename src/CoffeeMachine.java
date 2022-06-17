efd6465ddefa265b530cff1c143da3a1b64594e4
aleksandrgrigorev <xundr1992@gmail.com>
2022-06-17 20:27:41 +0400

    Stage 5-6/6: Keep track of the supplies & Brush up your code

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,121 +1,175 @@
 import java.util.Scanner;
 
 public class CoffeeMachine {
-    private static final int WATER_AT_START = 400;
-    private static final int MILK_AT_START = 540;
-    private static final int COFFEE_AT_START = 120;
-    private static final int CUPS_AT_START = 9;
-    private static final int MONEY_AT_START = 550;
 
-    private static final int CUPS_LEFT = 8;
-    private static final int MONEY_LEFT = 0;
+    private static final State state = new State();
 
     public static void main(String[] args) {
-        printMachineState(WATER_AT_START, MILK_AT_START, COFFEE_AT_START, CUPS_AT_START, MONEY_AT_START);
         System.out.println();
-        System.out.println("Write action (buy, fill, take):");
-        Scanner scanner = new Scanner(System.in);
-        String action = scanner.next();
 
-        switch (action) {
-            case "take":
-                take();
-                break;
-            case "fill":
-                fill(scanner);
-                break;
-            case "buy":
-                buy(scanner);
-                break;
-            default:
-                break;
+        while (true) {
+            System.out.println("Write action (buy, fill, take, remaining, exit):");
+            Scanner scanner = new Scanner(System.in);
+            String action = scanner.next();
+
+            switch (action) {
+                case "take":
+                    take();
+                    break;
+                case "fill":
+                    fill(scanner);
+                    break;
+                case "buy":
+                    buy(scanner);
+                    break;
+                case "remaining":
+                    state.print();
+                    break;
+                case "exit":
+                    return;
+                default:
+                    break;
+            }
         }
     }
 
     private static void take() {
-        System.out.println("I gave you $" + MONEY_AT_START);
+        int money = state.takeAllMoney();
+        System.out.println("I gave you $" + money);
         System.out.println();
-        printMachineState(WATER_AT_START, MILK_AT_START, COFFEE_AT_START, CUPS_AT_START, MONEY_LEFT);
     }
 
     private static void fill(Scanner scanner) {
         System.out.println("Write how many ml of water you want to add:");
-        int addWater = scanner.nextInt();
-        addWater = WATER_AT_START + addWater;
+        int waterToAdd = scanner.nextInt();
+        state.addWater(waterToAdd);
         System.out.println("Write how many ml of milk you want to add:");
-        int addMilk = scanner.nextInt();
-        addMilk = MILK_AT_START + addMilk;
+        int milkToAdd = scanner.nextInt();
+        state.addMilk(milkToAdd);
         System.out.println("Write how many grams of coffee beans you want to add:");
-        int addCoffee = scanner.nextInt();
-        addCoffee = COFFEE_AT_START + addCoffee;
+        int coffeeToAdd = scanner.nextInt();
+        state.addCoffee(coffeeToAdd);
         System.out.println("Write how many disposable cups of coffee you want to add:");
-        int addCups = scanner.nextInt();
-        addCups = CUPS_AT_START + addCups;
+        int cupsToAdd = scanner.nextInt();
+        state.addCups(cupsToAdd);
         System.out.println();
-        printMachineState(addWater, addMilk, addCoffee, addCups, MONEY_AT_START);
     }
 
     private static void buy(Scanner scanner) {
-        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
-        int coffeeType = scanner.nextInt();
+        System.out.println();
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
+        String coffeeType = scanner.next();
         switch (coffeeType) {
-            case 1:
+            case "1":
                 espresso();
                 break;
-            case 2:
+            case "2":
                 latte();
                 break;
-            case 3:
+            case "3":
                 cappuccino();
                 break;
+            case "back":
+                return;
             default:
                 break;
         }
-    }
-
-    private static void printMachineState(int water, int milk, int coffee, int cups, int money) {
-        System.out.println("The coffee machine has:");
-        System.out.println(water + " ml of water");
-        System.out.println(milk + " ml of milk");
-        System.out.println(coffee + " g of coffee beans");
-        System.out.println(cups + " disposable cups");
-        System.out.println("$" + money + " of money");
     }
 
     private static void espresso() {
         int waterNeed = 250;
         int milkNeed = 0;
         int coffeeNeed = 16;
+        int cupsNeed = 1;
         int cost = 4;
 
-        printCoffee(waterNeed, milkNeed, coffeeNeed, cost);
+        makeCoffee(waterNeed, milkNeed, coffeeNeed, cupsNeed, cost);
     }
 
     private static void latte() {
         int waterNeed = 350;
         int milkNeed = 75;
         int coffeeNeed = 20;
+        int cupsNeed = 1;
         int cost = 7;
 
-        printCoffee(waterNeed, milkNeed, coffeeNeed, cost);
+        makeCoffee(waterNeed, milkNeed, coffeeNeed, cupsNeed, cost);
     }
 
     private static void cappuccino() {
         int waterNeed = 200;
         int milkNeed = 100;
         int coffeeNeed = 12;
+        int cupsNeed = 1;
         int cost = 6;
 
-        printCoffee(waterNeed, milkNeed, coffeeNeed, cost);
+        makeCoffee(waterNeed, milkNeed, coffeeNeed, cupsNeed, cost);
     }
 
-    private static void printCoffee(int waterNeed, int milkNeed, int coffeeNeed, int cost) {
-        int waterLeft = WATER_AT_START - waterNeed;
-        int milkLeft = MILK_AT_START - milkNeed;
-        int coffeeLeft = COFFEE_AT_START - coffeeNeed;
-        int moneyLeft = MONEY_AT_START + cost;
+    private static void makeCoffee(int waterNeed, int milkNeed, int coffeeNeed, int cupsNeed, int cost) {
+
+        if (state.waterLeft < waterNeed) {
+            System.out.println("Sorry, not enough water!");
+        } else if (state.milkLeft < milkNeed) {
+            System.out.println("Sorry, not enough milk!");
+        } else if (state.coffeeLeft < coffeeNeed) {
+            System.out.println("Sorry, not enough coffee!");
+        } else if (state.cupsLeft < cupsNeed) {
+            System.out.println("Sorry, not enough cups!");
+        } else {
+            System.out.println("I have enough resources, making you a coffee!");
+            state.change(waterNeed, milkNeed, coffeeNeed, cupsNeed, cost);
+        }
 
         System.out.println();
-        printMachineState(waterLeft, milkLeft, coffeeLeft, CUPS_LEFT, moneyLeft);
+    }
+
+    private static class State {
+        private int waterLeft = 400;
+        private int milkLeft = 540;
+        private int coffeeLeft = 120;
+        private int cupsLeft = 9;
+        private int moneyLeft = 550;
+
+        public void change(int waterNeed, int milkNeed, int coffeeNeed, int cupsNeed, int money) {
+            waterLeft = waterLeft - waterNeed;
+            milkLeft = milkLeft - milkNeed;
+            coffeeLeft = coffeeLeft - coffeeNeed;
+            cupsLeft = cupsLeft - cupsNeed;
+            moneyLeft = moneyLeft + money;
+        }
+
+        public int takeAllMoney() {
+            int money = moneyLeft;
+            moneyLeft = 0;
+            return money;
+        }
+
+        public void print() {
+            System.out.println();
+            System.out.println("The coffee machine has:");
+            System.out.println(waterLeft + " ml of water");
+            System.out.println(milkLeft + " ml of milk");
+            System.out.println(coffeeLeft + " g of coffee beans");
+            System.out.println(cupsLeft + " disposable cups");
+            System.out.println("$" + moneyLeft + " of money");
+            System.out.println();
+        }
+
+        public void addWater(int waterToAdd) {
+            waterLeft = waterLeft + waterToAdd;
+        }
+
+        public void addMilk(int milkToAdd) {
+            milkLeft = milkLeft + milkToAdd;
+        }
+
+        public void addCoffee(int coffeeToAdd) {
+            coffeeLeft = coffeeLeft + coffeeToAdd;
+        }
+
+        public void addCups(int cupsToAdd) {
+            cupsLeft = cupsLeft + cupsToAdd;
+        }
     }
 }
