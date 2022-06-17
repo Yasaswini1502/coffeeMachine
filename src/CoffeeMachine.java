09bee19fd1bf6bcd7c797bd46af36e44664084fa
aleksandrgrigorev <xundr1992@gmail.com>
2022-06-17 20:20:52 +0400

    Stage 3/6: Estimate the number of servings

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -4,15 +4,33 @@ public class CoffeeMachine {
     public static void main(String[] args) {
         int waterPerCup = 200;
         int milkPerCup = 50;
-        int beansPerCup = 15;
+        int coffeePerCup = 15;
+
+        System.out.println("Write how many ml of water the coffee machine has:");
+        Scanner scanner = new Scanner(System.in);
+        int hasWater = scanner.nextInt();
+
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int hasMilk = scanner.nextInt();
+
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int hasCoffee = scanner.nextInt();
 
         System.out.println("Write how many cups of coffee you will need:");
-        Scanner scanner = new Scanner(System.in);
+        int cupsNeeded = scanner.nextInt();
 
-        int cups = scanner.nextInt();
-        System.out.println("For " + cups + " cups of coffee you will need:");
-        System.out.println(waterPerCup * cups + " ml of water");
-        System.out.println(milkPerCup * cups + " ml of milk");
-        System.out.println(beansPerCup * cups + " g of coffee beans");
+        int howManyCupsWater = hasWater / waterPerCup;
+        int howManyCupsMilk = hasMilk / milkPerCup;
+        int howManyCupsCoffee = hasCoffee / coffeePerCup;
+        int cupsCanMake = Math.min(Math.min(howManyCupsWater, howManyCupsMilk), howManyCupsCoffee);
+        int howMoreCups = cupsCanMake - cupsNeeded;
+
+        if (howMoreCups == 0) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (howMoreCups > 0) {
+            System.out.println("Yes, I can make that amount of coffee (and even " + howMoreCups + " more than that)");
+        } else if (howMoreCups < 0) {
+            System.out.println("No, I can make only " + cupsCanMake + " cup(s) of coffee");
+        }
     }
 }
